924f2e0a12c442533b99c3c30869f08ee675fc59
ABandonLoves <a_bandon_love@outlook.com>
2026-08-17 17:02:37 +0800

    维度献祭

--- src/components/modals/SacrificeModal.vue
+++ src/components/modals/SacrificeModal.vue
@@ -18,18 +18,13 @@ export default {
     message() {
       if (Achievement(118).isUnlocked && !Pelle.isDoomed && (!player.disablePostReality || (Alpha.isRunning && Alpha.currentStage >= 12) ||
         (LHC.voidRunning && NullUpgrade.limerick1.isBought))) {
-        return `Dimensional Sacrifice will give you a boost to the 8th Antimatter Dimension based on the amount of
-          1st Antimatter Dimensions you had at the time of Sacrificing.`;
+        return `维度献祭会给予第八维度一个基于献祭时第一维度数量的加成。`;
       }
-      return `Dimensional Sacrifice will remove all of your 1st through 7th Antimatter Dimensions
-        (with the cost and multiplier unchanged), for a boost to the 8th Antimatter Dimension based on the total
-        amount of 1st Antimatter Dimensions sacrificed. It will take time to regain production.`;
+      return `维度献祭将会清空第一至第七维度（不改变价格和倍率）以给予第八维度一个加成，这一加成基于已献祭第一维度的数量。恢复生产需要时间。`;
     },
     multiplierText() {
-      if (Ascensions.sacA.isUnlocked) return `Power is currently ${formatPow(this.currentPower, 2, 3)} and will increase to
-        ${formatPow(this.nextPower, 2, 3)} on Dimensional Sacrifice.`;
-      return `Multiplier is currently ${formatX(this.currentMultiplier, 2, 2)} and will increase to
-        ${formatX(this.nextMultiplier, 2, 2)} on Dimensional Sacrifice.`;
+      if (Ascensions.sacA.isUnlocked) return `当前献祭指数为 ${formatPow(this.currentPower, 2, 3)}，下次献祭后将提高至 ${formatPow(this.nextPower, 2, 3)}。`;
+      return `当前献祭倍率为 ${formatX(this.currentMultiplier, 2, 2)}，下次献祭后将提高至${formatX(this.nextMultiplier, 2, 2)}。`;
     },
   },
   methods: {
