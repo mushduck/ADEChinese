1dcabc534319439166a0ef6bfe9f7ad8ac3a8831
Supersonic Seven <b.rockholz@icloud.com>
2026-09-02 23:44:10 -0400

    Update logCost calculation for distant scaling

--- src/core/replicanti.js
+++ src/core/replicanti.js
@@ -720,7 +720,7 @@ export const ReplicantiUpgrade = {
         // So, the difference between successive scales goes 5, 5, 5, 255, 55, 55, ...
         const extraIncrements = 5;
         const numDistant = count.sub(distantReplicatedGalaxyStart);
-        logCost = logCost.add(new Decimal(logRemoteScaling).times(numRemote).times(numRemote.add(1)).times(numRemote.times(2).add(1)).div(6));
+        logCost = logCost.add(new Decimal(logDistantScaling).times(numDistant).times(numDistant.add(2 * extraIncrements).sub(1)).div(2));
       }
       if (count.gt(remoteReplicatedGalaxyStart)) {
         const logRemoteScaling = 5;
